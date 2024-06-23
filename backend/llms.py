8efd18182d6e7656a236568e6af4eceab1166726
aaron10l <aaronalvarez7394@gmail.com>
2024-06-22 17:29:01 -0700

    add GPT api call

--- backend/llms.py
+++ backend/llms.py
@@ -1,12 +1,29 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
 def get_gpt_response(prompt):
-	print("gpt response")
-	return "GPT RESPONSE"
+	print(f"prompt: {prompt}")
+
+	client = OpenAI(
+    	api_key=os.getenv('GPT_KEY')
+	)
+	
+	completion = client.chat.completions.create(
+  		model="gpt-3.5-turbo",
+  		messages=[
+    		{"role": "user", "content": prompt}
+  		]
+	)
+
+	response = completion.choices[0].message.content
+	return response
 
 def get_llama_response(prompt):
-	print("llama response")
 	return "llama response"
 
 def get_claude_response(prompt):
-	print("claude response")
 	return "claude RESPONSE"
 
